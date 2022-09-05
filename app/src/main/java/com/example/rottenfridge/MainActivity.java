336469402868df87528499aadacd218eee02217f
mattiabottini <mattia.bottini01@universitadipavia.it>
2022-09-05 16:24:30 +0200

    First Release

--- app/src/main/java/com/example/rottenfridge/MainActivity.java
+++ app/src/main/java/com/example/rottenfridge/MainActivity.java
@@ -24,14 +24,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.journeyapps.barcodescanner.ScanContract;
-import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.util.ArrayList;
 
@@ -49,21 +41,11 @@ public class MainActivity extends AppCompatActivity {
     CustomAdapter customAdapter;
     ImageView empty;
     TextView textEmpty;
-    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
         recyclerView=findViewById(R.id.recyclerView);
         toolbar=findViewById(R.id.Toolbar);
         setSupportActionBar(toolbar);
