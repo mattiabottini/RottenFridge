563689b1add81e7e2bf0838c4bd24e2ddf97bae3
mattiabottini <mattia.bottini01@universitadipavia.it>
2022-09-10 13:31:45 +0200

    Speech to Text

--- app/src/main/java/com/MattiaBottini/rottenfridge/AddActivity.java
+++ app/src/main/java/com/MattiaBottini/rottenfridge/AddActivity.java
@@ -1,19 +1,26 @@
 package com.MattiaBottini.rottenfridge;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -23,6 +30,10 @@ public class AddActivity extends AppCompatActivity {
     EditText name_input, expiration_input, quantity_input;
     Button add_button;
     Toolbar toolbar;
+    private static final int RESULT_SPEECH = 1;
+    private FloatingActionButton micButtonName, micButtonQuantity;
+    boolean button1, button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,41 @@ public class AddActivity extends AppCompatActivity {
         expiration_input=findViewById(R.id.product_expirationDate);
         quantity_input=findViewById(R.id.product_quantity);
         add_button=findViewById(R.id.add_Button);
+
+        micButtonName = findViewById(R.id.micButtonName);
+        micButtonQuantity = findViewById(R.id.micButtonQuantity);
+        micButtonName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    name_input.setText("");
+                    button1=true;
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Your Device does not support Speech to Text!", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        micButtonQuantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    quantity_input.setText("");
+                    button2=true;
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Your Device does not support Speech to Text!", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
         toolbar=findViewById(R.id.Toolbar);
         toolbar.setTitle("Add Product");
         toolbar.setBackground(Drawable.createFromPath("#f2f4f8"));
@@ -80,4 +126,94 @@ public class AddActivity extends AppCompatActivity {
         SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.ITALIAN);
         expiration_input.setText(dateFormat.format(myCalendar.getTime()));
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_SPEECH:
+                if(resultCode==RESULT_OK && data!=null){
+                    if(button1) {
+                        ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        name_input.setText(text.get(0));
+                        button1=false;
+                    } else if(button2){
+                        ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        String intFound = getNumberFromResult(text);
+                        if (!isNumeric(intFound)) {
+                            quantity_input.setText("");
+                            button2=false;
+                        } else {
+                            quantity_input.setText(String.valueOf(intFound));
+                            button2=false;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    // method to loop through results trying to find a number
+    private String getNumberFromResult(ArrayList<String> results) {
+        for (String str : results) {
+            if (getIntNumberFromText(str.toLowerCase()) != "") {
+                return getIntNumberFromText(str);
+            }
+        }
+        return "";
+    }
+
+    // method to convert string number to integer
+    private String getIntNumberFromText(String strNum) {
+        switch (strNum) {
+            case "zero":
+                return "0";
+            case "uno":
+            case "one":
+            case "una":
+                return "1";
+            case "due":
+            case "two":
+            case "to":
+                return "2";
+            case "tre":
+            case "three":
+            case "free":
+                return "3";
+            case "quattro":
+            case "four":
+            case "for":
+                return "4";
+            case "cinque":
+            case "five":
+                return "5";
+            case "sei":
+            case "six":
+                return "6";
+            case "sette":
+            case "seven":
+                return "7";
+            case "otto":
+            case "eight":
+                return "8";
+            case "nove":
+            case "nine":
+                return "9";
+        }
+        return strNum;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
 }
+
